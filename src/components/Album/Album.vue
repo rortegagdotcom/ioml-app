39ee57252351f1d8f505b91dc81323c31e647b0c
Raúl Ortega <raulortegagallego48@hotmail.es>
2023-02-25 18:52:15 +0100

    IOML APP - WP

--- src/components/Album/Album.vue
+++ src/components/Album/Album.vue
@@ -6,12 +6,35 @@ const props = defineProps({
 
 <template>
   <div
-    :class="`block w-80 h-80 p-5 bg-${album.cover} border-gray-200 rounded-lg shadow dark:border-gray-700 cursor-pointer`"
+    :class="`bg-${album.cover}`"
+    class="block w-80 h-80 p-5 border-gray-200 rounded-lg shadow dark:border-gray-700 cursor-pointer"
   >
-    <h3 class="mb-2 text-2xl font-bold text-center">
-      {{ album.name }}
-    </h3>
+    <div class="bg-gray-100 dark:bg-gray-900 p-5 rounded-xl">
+      <h3 class="mb-2 text-2xl font-bold text-center">
+        {{ album.name }}
+      </h3>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+/* Se importan estilos CSS personalizado, por que no pone el background-image con TailwindCSS en su configuración */
+@import './css/covers/bank-note.css';
+@import './css/covers/bricks.css';
+@import './css/covers/bubbles.css';
+@import './css/covers/circles.css';
+@import './css/covers/cross.css';
+@import './css/covers/diamonds.css';
+@import './css/covers/domino.css';
+@import './css/covers/dots.css';
+@import './css/covers/hexagons.css';
+@import './css/covers/jigsaw.css';
+@import './css/covers/jupiter.css';
+@import './css/covers/leafs.css';
+@import './css/covers/melt.css';
+@import './css/covers/squares.css';
+@import './css/covers/stripes.css';
+@import './css/covers/tic-tac-toe.css';
+@import './css/covers/triangles.css';
+@import './css/covers/yyy.css';
+</style>
--- src/components/IOMLTab/IOMLTab.vue
+++ src/components/IOMLTab/IOMLTab.vue
@@ -52,6 +52,12 @@ function handleCoverClick(cover) {
   cover = cover.substring(cover.lastIndexOf('/') + 1, cover.length - 4);
   emit('cover-clicked', cover);
 }
+
+const selectedIndex = ref(null);
+
+function selectImage(index) {
+  selectedIndex.value = index;
+}
 </script>
 
 <template>
@@ -77,92 +83,290 @@ function handleCoverClick(cover) {
   <div class="py-4">
     <div class="grid grid-cols-10" v-if="activeTab === 0">
       <div v-for="(cover, index) in coversBankNote" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 1">
       <div v-for="(cover, index) in coversBricks" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 2">
       <div v-for="(cover, index) in coversBubbles" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 3">
       <div v-for="(cover, index) in coversCircles" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 4">
       <div v-for="(cover, index) in coversCross" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 5">
       <div v-for="(cover, index) in coversDiamonds" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 6">
       <div v-for="(cover, index) in coversDomino" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 7">
       <div v-for="(cover, index) in coversDots" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 8">
       <div v-for="(cover, index) in coversHexagons" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 9">
       <div v-for="(cover, index) in coversJigsaw" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 10">
       <div v-for="(cover, index) in coversJupiter" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 11">
       <div v-for="(cover, index) in coversLeafs" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 12">
       <div v-for="(cover, index) in coversMelt" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 13">
       <div v-for="(cover, index) in coversSquares" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 14">
       <div v-for="(cover, index) in coversStripes" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 15">
       <div v-for="(cover, index) in coversTicTacToe" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 16">
       <div v-for="(cover, index) in coversTriangles" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
     <div class="grid grid-cols-10" v-if="activeTab === 17">
       <div v-for="(cover, index) in coversYYY" :key="index">
-        <img class="w-32" :src="cover" @click="handleCoverClick(cover)" />
+        <img
+          class="w-32 cursor-pointer"
+          :src="cover"
+          @click="
+            handleCoverClick(cover);
+            selectImage(index);
+          "
+          :class="{
+            'border-8 border-solid border-pink-500 dark:border-orange-500':
+              selectedIndex == index,
+          }"
+        />
       </div>
     </div>
   </div>
