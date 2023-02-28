02476ff197eb5ee97b4ad4bfe640ff928efa5b6c
Raúl Ortega <raulortegagallego48@hotmail.es>
2023-02-28 19:58:38 +0100

    IOML APP - WP

--- src/components/Album/AlbumDelete.vue
+++ src/components/Album/AlbumDelete.vue
@@ -1,9 +1,27 @@
 <script setup>
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
+import axios from 'axios';
+import { useRouter, useRoute } from 'vue-router';
 
+const router = useRouter();
 const route = useRoute();
-const albumId = computed(() => route.params.id);
+const albumId = route.params.id;
+
+function goBack() {
+  router.push('/');
+}
+
+async function handleDeleteAlbum() {
+  await axios
+    .delete(`http://localhost:5748/api/albums/${albumId}`)
+    .then(() => {
+      console.log('Album deleted');
+    })
+    .catch((error) => {
+      console.log('Error when deleting album', error);
+    });
+
+  router.push('/');
+}
 </script>
 
 <template>
@@ -12,11 +30,13 @@ const albumId = computed(() => route.params.id);
     <div class="flex flex-wrap flex-row justify-around">
       <button
         class="text-white bg-gradient-to-br from-pink-500 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2"
+        @click.prevent="goBack()"
       >
         No
       </button>
       <button
         class="text-white bg-gradient-to-br from-pink-500 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2"
+        @click.prevent="handleDeleteAlbum()"
       >
         Yes
       </button>
